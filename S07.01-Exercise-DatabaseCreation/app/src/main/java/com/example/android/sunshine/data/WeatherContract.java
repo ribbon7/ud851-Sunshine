56678c5fbfbf43be7855e586027667b6208915f2
Connor Earl <connor@manonfireent.com>
2017-03-30 19:51:38 -0600

    Complete S07.01

--- S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
+++ S07.01-Exercise-DatabaseCreation/app/src/main/java/com/example/android/sunshine/data/WeatherContract.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.sunshine.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Defines table and column names for the weather database. This class is not necessary, but keeps
  * the code organized.
@@ -41,4 +43,16 @@ public class WeatherContract {
 //      TODO (9) Create a public static final String call COLUMN_WIND_SPEED with the value "wind"
 
 //      TODO (10) Create a public static final String call COLUMN_DEGREES with the value "degrees"
+
+    public static final class WeatherEntry implements BaseColumns{
+        public static final String TABLE_NAME = "weather";
+        public static final String COLUMN_DATE = "date";
+        public static final String COLUMN_WEATHER_ID = "weather_id";
+        public static final String COLUMN_MIN_TEMP = "min";
+        public static final String COLUMN_MAX_TEMP = "max";
+        public static final String COLUMN_HUMIDITY = "humidity";
+        public static final String COLUMN_PRESSURE = "pressure";
+        public static final String COLUMN_WIND_SPEED = "wind";
+        public static final String COLUMN_DEGREES = "degrees";
+    }
 }
