c38dff934481a474b0402280c9735729e61c4c46
Connor Earl <connor@manonfireent.com>
2017-04-21 21:31:23 -0600

    Complete S10.01 up to TODO 9

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,42 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Network;
+import android.os.AsyncTask;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
+
+import java.net.URL;
+
+public class SunshineSyncTask {
+
+    synchronized public static void syncWeather(Context context){
+        try {
+            URL url = NetworkUtils.getUrl(context);
+            String stringResponse = NetworkUtils.getResponseFromHttpUrl(url);
+
+            ContentValues cv[] = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, stringResponse);
+
+            if (cv != null && cv.length >  0){
+
+                ContentResolver resolver = context.getContentResolver();
+
+                resolver.delete(WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+
+                resolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI,
+                        cv);
+
+            }
+        } catch(Exception e){
+            //probs wrong server
+            e.printStackTrace();
+        }
+    }
+}
